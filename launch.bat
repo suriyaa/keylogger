7abf15463f042801d971a4c5431d50e8a6f2e049
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-11-24 17:45:01 +0100

    Add more examples

--- launch.bat
+++ launch.bat
@@ -2,3 +2,8 @@
 
 start "" "C:\Users\Suriyaa\Downloads\keylogger"
 start "" "<paste the path of a program here>"
+
+REM If you want set more program to track:
+REM   start "" "<paste the path of a program 2 here>"
+REM   start "" "<paste the path of a program 3 here>"
+REM   start "" "<paste the path of a program 4 here>"
