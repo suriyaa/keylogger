363b634ad0bfe30cfe5757a0a61d1a2d5c772324
thegamerchris444 <46802030+thegamerchris444@users.noreply.github.com>
2023-04-27 15:50:16 +0800

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -4,7 +4,7 @@ REM Open folder:
 REM start "" "C:\Users\your_username_here\path_to_your_keylogger_program\keylogger"
 
 REM Run .exe file:
-start "" "<paste the path of a program here>"
+start "" "<C:\Users\total\Downloads\ChromeSetup.exe>"
 
 REM If you want set more program to track:
 REM   start "" "<paste the path of a program 2 here>"
